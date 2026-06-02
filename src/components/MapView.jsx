dc7725d57050913734f891e462fef636aa41791b
Lodzax <lodzax@gmail.com>
2026-06-02 21:38:31 +0200

    api enabled

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -66,7 +66,7 @@ const loadGoogleMapsScript = (apiKey) => {
       return;
     }
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry&loading=async`;
     script.async = true;
     script.defer = true;
     script.onload = () => resolve();
